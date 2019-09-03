be5efe22d69d08b4413c0ced6951f6d37fac6675
Hernan Chilabert <chila6@gmail.com>
2019-09-03 00:56:29 -0300

    Django REST Framework - Instalacion del package - Creacion de funciones para lista y crear circulos

--- cride/circles/views.py
+++ cride/circles/views.py
@@ -1,18 +1,20 @@
 """Circles views."""
 
-# Django
-from django.http import JsonResponse
+# Django REST Framework
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Models
 from cride.circles.models import Circle
 
 
+@api_view(['GET'])
 def list_circles(request):
     """List circles."""
-    circles = Circle.objects.all()
-    public = circles.filter(is_public=True) # Hasta esta linea no se ejecuto SQL en la base
+    circles = Circle.objects.filter(is_public=True)
+    # public = circles.filter(is_public=True) # Hasta esta linea no se ejecuto SQL en la base
     data = []
-    for circle in public:
+    for circle in circles:
         data.append({
             'name': circle.name,
             'slug_name': circle.slug_name,
@@ -21,4 +23,21 @@ def list_circles(request):
             'members_limit': circle.members_limit
         })
 
-    return JsonResponse(data, safe=False)
+    return Response(data)
+
+
+@api_view(['POST'])
+def create_circle(request):
+    """Create circle."""
+    name = request.data['name']
+    slug_name = request.data['slug_name']
+    about = request.data.get('about', '')
+    circle = Circle.objects.create(name=name, slug_name=slug_name, about=about)
+    data = {
+        'name': circle.name,
+        'slug_name': circle.slug_name,
+        'rides_taken': circle.rides_taken,
+        'rides_offered': circle.rides_offered,
+        'members_limit': circle.members_limit
+    }
+    return Response(data)
