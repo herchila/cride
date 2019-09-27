f05423ea2fea1d04004d56c356991d97a6c779c1
Hernan Chilabert <chila6@gmail.com>
2019-09-26 23:52:22 -0300

    User login API

---
+++ cride/users/urls.py
@@ -0,0 +1,12 @@
+"""User's URLs."""
+
+# Django
+from django.urls import path
+
+# Views
+from cride.circles.views import UserLoginAPIView
+
+
+urlpatterns = [
+    path('users/login/', UserLoginAPIView.as_view(), name='login'),
+]
