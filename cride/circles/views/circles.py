89d05a66c7310f710be74d7931faf348606cf236
Hernan Chilabert <chila6@gmail.com>
2019-10-15 03:50:51 -0300

    Authentication & Pagination

---
+++ cride/circles/views/circles.py
@@ -0,0 +1,24 @@
+"""Circle views."""
+
+# Django REST Framework
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+# Serializers
+from cride.circles.serializers import CircleModelSerializer
+
+# Models
+from cride.circles.models import Circle
+
+
+class CircleViewSet(viewsets.ModelViewSet):
+    """Circle view set."""
+
+    serializer_class = CircleModelSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        queryset = Circle.objects.all()
+        if self.action == 'list':
+            return queryset.filter(is_public=True)
+        return queryset
