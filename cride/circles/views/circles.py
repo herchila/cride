22274fdfee4971651bf34165126666216484e440
Hernan Chilabert <chila6@gmail.com>
2019-10-24 01:50:33 -0300

    Alta de circulos y permisos

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -1,24 +1,49 @@
 """Circle views."""
 
 # Django REST Framework
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
+
+# Permissions
 from rest_framework.permissions import IsAuthenticated
+from cride.circles.permissions.circles import IsCircleAdmin
 
 # Serializers
 from cride.circles.serializers import CircleModelSerializer
 
 # Models
-from cride.circles.models import Circle
+from cride.circles.models import Circle, Membership
 
 
-class CircleViewSet(viewsets.ModelViewSet):
+class CircleViewSet(mixins.CreateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
     """Circle view set."""
 
     serializer_class = CircleModelSerializer
-    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         queryset = Circle.objects.all()
         if self.action == 'list':
             return queryset.filter(is_public=True)
         return queryset
+
+    def get_permissions(self):
+        permissions = [IsAuthenticated]
+        if self.action in ['update', 'partial_update']:
+            permissions.append(IsCircleAdmin)
+        return [permission() for permission in permissions]
+
+    def perform_create(self, serializer):
+        """Assign circle admin."""
+        circle = serializer.save()
+        user = self.request.user
+        profile = user.profile
+        Membership.objects.create(
+            user=user,
+            profile=profile,
+            circle=circle,
+            is_admin=True,
+            remaining_invitations=10
+        )
