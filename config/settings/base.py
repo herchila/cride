e7738e427eb65a47e07acd3247a792559a76c72e
Hernan Chilabert <chila6@gmail.com>
2019-12-30 02:41:10 -0300

    [WIP]: List members - Recursos anidado

--- config/settings/base.py
+++ config/settings/base.py
@@ -161,6 +161,7 @@ CELERYD_TASK_SOFT_TIME_LIMIT = 60
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer'
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
