fe2f737d9aa5a24c5f1d0df2de1eb176c01c8da6
Hernan Chilabert <chila6@gmail.com>
2020-01-01 10:01:22 -0300

    Retrieve destroy member

--- cride/circles/serializers/memberships.py
+++ cride/circles/serializers/memberships.py
@@ -14,6 +14,7 @@ class MembershipModelSerializer(serializers.ModelSerializer):
     """Membership model serializer."""
 
     user = UserModelSerializer(read_only=True)
+    invited_by = serializers.StringRelatedField()
     joined_at = serializers.DateTimeField(source='created', read_only=True)
 
     class Meta:
